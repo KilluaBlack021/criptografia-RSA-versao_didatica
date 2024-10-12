21f884d6200cecde27bb43b6163afed6e14f62c3
José Mayco <116230868+KilluaBlack021@users.noreply.github.com>
2024-10-12 13:00:14 -0300

    codigo base

--- main.py
+++ main.py
@@ -1,1 +1,58 @@
-print('Olá, mundo!')
+from random import randrange
+
+
+def mdc(n1, n2):
+    while n2 != 0:
+        r = n1 % n2
+        n1 = n2
+        n2 = r
+    return n1
+
+
+def gerador_chave_publica(n):
+    while True:
+        e = randrange(2, n)
+        if mdc(n, e) == 1:
+            return e
+
+
+def gerador_chave_privada(totiente, e):
+    d = 0
+    while d*e % totiente != 1:
+        d += 1
+    return d
+
+
+def cifrar(msg, e, n):
+    msg_cifrada = ''
+    for letra in msg:
+        k = ord(letra) ** e % n
+        msg_cifrada += chr(k)
+    return msg_cifrada
+
+
+def decifrar(msg, n, d):
+    msg_decifrada = ''
+    for letra in msg:
+        k = ord(letra) ** d % n
+        msg_decifrada += chr(k)
+    return msg_decifrada
+
+
+
+# main
+msg = ''
+p = 17
+q = 19
+n = p * q
+totiente = (p-1) * (q-1)   # phi(n)
+e = gerador_chave_publica(totiente)
+d = gerador_chave_privada(totiente, e)
+print(f'''
+chave publica: {e}, {n}
+chave privada: {d, n}
+''')
+msg = cifrar(msg, e, n)
+print(f'msg cifrada: {msg}\n')
+msg = decifrar(msg, n, d)
+print(f'msg decifrada: {msg}\n')
