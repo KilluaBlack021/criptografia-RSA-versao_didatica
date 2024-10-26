3280f9e611cc288cf681f5278386cb70eb917ab8
José Mayco <116230868+KilluaBlack021@users.noreply.github.com>
2024-10-26 17:13:15 -0300

    TIrei versão complexa
    Retirei o arquivo de criptografia que trabalhava com arquivo e pa.
    
    Vou deixar esse projeto mais voltado a questão didatica do negocio e vou trabalhar no outro por fora.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import randrange
 
 
-def mdc(n1, n2):
+def mdc(n1, n2): # algoritmo de euclides
     while n2 != 0:
         r = n1 % n2
         n1 = n2
@@ -16,13 +16,6 @@ def gerador_chave_publica(totiente):
             return e
 
 
-def gerador_chave_privada(totiente, e):
-    d = 0
-    while d*e % totiente != 1:
-        d += 1
-    return d
-
-
 def cifrar(msg, e, n):
     msg_cifrada = ''
     for letra in msg:
@@ -31,27 +24,44 @@ def cifrar(msg, e, n):
     return msg_cifrada
 
 
+def gerador_chave_privada(totiente, e):
+    d = 0
+    while d*e % totiente != 1:
+        d += 1
+    return d
+
+
 def decifrar(msg, n, d):
     msg_decifrada = ''
+
     for letra in msg:
         k = ord(letra) ** d % n
         msg_decifrada += chr(k)
+
     return msg_decifrada
 
 
 # main
 msg = '''
-Era uma vez os três porquinhos, 
-o Bob, o Valdisney e o Pedrinho.
-Bob é viciado no cigarrinho do capeta
-sua casa é de palha e passa o dia na... trobeta!
-Valdisney é favelado, tem um barraco de madeira 
-graças ao bolsa familia não trampou a vida inteira.
-O Pedrinho é diferente, designin de interiores,
-tem uma casa de tijolos e limpa a ***** com flores'''
+Say your prayers, little one
+Don't forget, my son
+To include everyone
+I tuck you in, warm within
+Keep you free from sin
+'Til the sandman, he comes
+
+Sleep with one eye open
+Gripping your pillow tight
+
+Exit light
+Enter night
+Take my hand
+We're off to never-never land
+'''
 
 p = 17 # primo 1
 q = 19 # primo 2
+
 n = p * q
 totiente = (p-1) * (q-1) # inverso multiplicativo de N
 
@@ -59,9 +69,9 @@ e = gerador_chave_publica(totiente)
 d = gerador_chave_privada(totiente, e)
 
 
-#print(f'chave publica: {e}, {n}\n chave privada: {d, n}')
+print(f'Chave publica: {e}, {n}\n\nChave privada: {d, n}\n\n\n')
 
 msg = cifrar(msg, e, n)
-print(f'msg cifrada: {msg}\n')
+print(f'MSG CIFRADA: \n\n{msg}\n\n')
 msg = decifrar(msg, n, d)
-print(f'msg decifrada: {msg}\n')
+print(f'MSG DECIFRADA: \n\n{msg}\n\n')
