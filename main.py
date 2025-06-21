cbdaa66db6ff42dc8bbddecdb74d71cf89d894b0
José Mayco <116230868+KilluaBlack021@users.noreply.github.com>
2025-06-20 21:33:51 -0300

    de phi_n para z
    mudança do nome da variavel "phi_n" para "Z". Assim eu torno o negocio mais matematicamente "correto"

--- main.py
+++ main.py
@@ -90,10 +90,10 @@ p = 53 # primo 1      exemplos de primos: 11 13 17 19 23 29 31 37 41 43 47 53 59
 q = 137 # primo 2                        107 109 113 127 131 137 139 149 151 157 163 167 173 179 181 191 193 197
 
 n = p * q
-phi_n = (p-1) * (q-1) # totiente de N
+z = (p-1) * (q-1) # totiente de N
 
 e = 65537 # gerador_chave_publica(phi_n) # motivo 1 pelo qual usei esse número: https://pt.wikipedia.org/wiki/65537; Motivo 2: é um número primo grande o suficiente, mas não perde eficiência na hora dos calculos
-d = gerador_chave_privada(phi_n, e)
+d = gerador_chave_privada(z, e)
 
 
 print(f'Chave publica: {e}, {n}\n\nChave privada: {d, n}\n\n\n')
